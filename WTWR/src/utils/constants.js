e886be0a9d05bea55ba768226d727ad463f846bd
Luis Carrola <lcarrola70@gmail.com>
2025-01-05 22:45:40 -0700

    Api started

--- WTWR/src/utils/constants.js
+++ WTWR/src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 40.39439,
+  longitude: -105.07058,
+};
+
+export const APIkey = "8c35146fac11e216d98b71a16b88e129";
