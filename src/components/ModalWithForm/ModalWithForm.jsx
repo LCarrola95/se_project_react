a2fe93d8074dd4c6c557e49c0e12e816ef794776
Luis Carrola <lcarrola70@gmail.com>
2025-01-31 19:04:59 -0700

    check response, duplication and redundant validation corrected

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,6 +1,6 @@
 import "./ModalWithForm.css";
 import closeButton from "../../assets/close-button.svg";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 function ModalWithForm({
   children,
@@ -11,42 +11,14 @@ function ModalWithForm({
   onSubmit,
   isLoading,
 }) {
-  const [isValid, setIsValid] = useState(false);
-  const [errors, setErrors] = useState({});
-
   useEffect(() => {
     if (!isOpen) {
-      setIsValid(false);
-      setErrors({});
-
       const form = document.querySelector(".modal__form");
       if (form) {
         form.reset();
       }
     }
   }, [isOpen]);
-
-  const handleFormChange = (e) => {
-    const form = e.target.form;
-    const radioButtons = form.querySelectorAll('input[type="radio"]');
-    const radioSelected = Array.from(radioButtons).some(
-      (radio) => radio.checked
-    );
-
-    const formErrors = {};
-    const nameInput = form.querySelector("#name");
-    const imageInput = form.querySelector("#imageUrl");
-
-    if (nameInput && nameInput.validationMessage) {
-      formErrors.name = nameInput.validationMessage;
-    }
-    if (imageInput && imageInput.validationMessage) {
-      formErrors.imageUrl = imageInput.validationMessage;
-    }
-
-    setErrors(formErrors);
-    setIsValid(form.checkValidity() && radioSelected);
-  };
 
   return (
     <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
@@ -64,17 +36,9 @@ function ModalWithForm({
             className="modal__close-icon"
           />
         </button>
-        <form
-          onSubmit={onSubmit}
-          onChange={handleFormChange}
-          className="modal__form"
-        >
-          {typeof children === "function" ? children(errors) : children}
-          <button
-            type="submit"
-            className={`modal__submit ${isValid ? "modal__submit_valid" : ""}`}
-            disabled={!isValid || isLoading}
-          >
+        <form onSubmit={onSubmit} className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit" disabled={isLoading}>
             {isLoading ? "Saving..." : buttonText}
           </button>
         </form>
